658c9a8b4934637a7b1d1a1bc3a3062988e0303e
Wilson Calderazzi <71198481+calderazzi@users.noreply.github.com>
2022-08-24 15:51:03 -0300

    mas funcionalidades

--- JS/empleados.js
+++ JS/empleados.js
@@ -25,19 +25,55 @@ function Ingresar() {
   )
   document.getElementById("form").style.display = "none"
   document.getElementById("articulos").style.display = "block"
-  Buscar();
+  
+  let checked = document.getElementById("articulos");
+  checked = addEventListener("change", Radios, false);
+  // radios = document.querySelector(`input[name="prioridad"]:checked`);
+}
+
+function Radios() {
+  let radios = document.querySelector(`input[name="prioridad"]:checked`);
+  if(radios.value == "productos") {
+    Buscar();
+  } else if (radios.value == "stock menor a mayor") {
+    OrdenarStock();
+  } else {
+    OrdenarPrecio();
+  }
 }
 
 function Buscar() {
-  let articulos = document.getElementById("articulos");
-  articulos.addEventListener(`click`, ()=> {
     let listaProd = document.getElementById("listaArticulos");
+    listaProd.innerHTML = "";
     arrayProductos.forEach(prod => { 
       let div = document.createElement(`div`);
       div.className = `content`;
-      div.innerHTML = `<p>Hay ${prod.stock} ${prod.nombre} en stock.</p>`
+      div.innerHTML = `<p>Cod: ${prod.codigo} - ${prod.stock} ${prod.nombre} ${prod.precio} </p>`
       listaProd.appendChild(div);
     })
-  })
+  
+}
+function OrdenarStock() {
+  let nuevoArray = arrayProductos.sort(((a,b)=> a.stock - b.stock));
+  let listaProd = document.getElementById("listaArticulos");
+  listaProd.innerHTML = "";
+  nuevoArray.forEach(prod => {
+    let div = document.createElement(`div`);
+      div.className = `content`;
+      div.innerHTML = `<p>Hay ${prod.stock} ${prod.nombre} en stock.</p>`
+      listaProd.appendChild(div);
+})
 }
 
+function OrdenarPrecio() {
+  let nuevoArray = arrayProductos.sort(((a,b)=> a.precio - b.precio));
+  let listaProd = document.getElementById("listaArticulos");
+  listaProd.innerHTML = "";
+  nuevoArray.forEach(prod => {
+    let div = document.createElement(`div`);
+      div.className = `content`;
+      div.innerHTML = `<p>${prod.stock} ${prod.nombre} $${prod.precio}</p>`
+      listaProd.appendChild(div);
+  })
+
+}
